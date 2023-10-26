5256f637bf975a8a317dd4eb10d3252c2c1872e3
romeusorionaet <romeusoares14569@gmail.com>
2023-10-26 14:08:35 -0300

    fix: timezone end add try catch

--- src/app/api/order/payment-success/route.ts
+++ src/app/api/order/payment-success/route.ts
@@ -4,24 +4,31 @@ import { NextResponse } from 'next/server'
 
 const stripe = initializeStripe()
 
-export const POST = async (request: Request) => {
-  const signature = request.headers.get('stripe-signature')
+export const POST = async (req: Request) => {
+  const signature = req.headers.get('stripe-signature')
 
   if (!signature) {
     return NextResponse.error()
   }
 
-  const text = await request.text()
+  const text = await req.text()
 
-  const event = stripe.webhooks.constructEvent(
-    text,
-    signature,
-    process.env.STRIPE_WEBHOOK_SECRET_KEY,
-  )
+  let event
 
-  console.log('========', event.type)
+  try {
+    event = stripe.webhooks.constructEvent(
+      text,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET_KEY,
+      400,
+    )
+  } catch (err) {
+    console.log('Deu Erro no event', err)
+  }
 
-  if (event.type === 'checkout.session.completed') {
+  console.log('========', event?.type)
+
+  if (event && event.type === 'checkout.session.completed') {
     console.log('====entrou aqui=======')
     const session = event.data.object as any
 
