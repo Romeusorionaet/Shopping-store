76639616c20186a77362e4cf6e72ee42b569d329
romeusorionaet <romeusoares14569@gmail.com>
2023-11-13 12:14:53 -0300

    fix: bug layout and rename slider products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
-import { DiscountedProducts } from '@/components/discounted-products'
 import { getDataProducts } from '@/lib/getData/get-data-products'
 import Link from 'next/link'
 
 import { LibraryBig } from 'lucide-react'
 import { OfferBanner } from '@/components/offer-banner'
+import { CarouselProducts } from '@/components/carousel-products'
 
 export default async function Home() {
   const { props } = await getDataProducts()
 
-  const filteredProductsWithDiscount = props.products.filter(
-    (product) => product.discountPercentage !== 0,
-  )
+  const filteredProductsWithDiscount = props.products
+    .filter((product) => product.discountPercentage !== 0)
+    .sort(() => Math.random() - 0.5)
+
+  const allProducts = props.products.sort(() => Math.random() - 0.5)
 
   return (
-    <div className="flex flex-col gap-6 h-screen overflow-hidden">
+    <div className="flex flex-col gap-6 overflow-hidden pb-8 max-w-[1480px] mx-auto">
       <div className="relative max-w-[1480px] w-full mx-auto">
         <div className="bg-gradient-to-r from-amber-100/40 h-full w-40 max-2xl:w-28 max-sm:w-10 absolute left-0 top-0 z-10" />
         <OfferBanner />
@@ -27,7 +29,17 @@ export default async function Home() {
         <LibraryBig size={16} />
       </div>
 
-      <DiscountedProducts products={filteredProductsWithDiscount} />
+      <div>
+        <h2 className="my-4 text-lg">Produtos em Promoção</h2>
+
+        <CarouselProducts products={filteredProductsWithDiscount} />
+      </div>
+
+      <div>
+        <h2 className="my-4 text-lg">Todos os Produtos</h2>
+
+        <CarouselProducts products={allProducts} />
+      </div>
     </div>
   )
 }
---
+++ src/components/carousel-products/index.tsx
@@ -0,0 +1,119 @@
+'use client'
+
+import { useKeenSliderMode } from '@/hooks/useKeenSliderMode'
+import { Product } from '@prisma/client'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
+import Image from 'next/image'
+
+interface productsProps {
+  products: Product[] | undefined
+}
+
+export function CarouselProducts({ products }: productsProps) {
+  const { sliderRef, currentSlide, instanceRef } = useKeenSliderMode()
+
+  return (
+    <div
+      ref={sliderRef}
+      className="keen-slider w-full max-w-[1400px] mx-auto px-4"
+    >
+      <div className="relative">
+        <div className="overflow-hidden flex">
+          {products &&
+            products.map((product) => {
+              const totalDiscount =
+                Number(product.basePrice) * (product.discountPercentage / 100)
+              const totalPrice = Number(product.basePrice) - totalDiscount
+
+              return (
+                <div key={product.id} className="keen-slider__slide z-20">
+                  <div className="p-4 bg-amber-50/60 hover:bg-amber-50 duration-700 flex flex-col justify-center items-center gap-2 rounded-md h-full lg:w-80 max-md:w-72 max-sm:w-52">
+                    <p className="text-sm">{product.name}</p>
+
+                    <div>
+                      {product.discountPercentage !== 0 && (
+                        <p className="text-xs line-through opacity-75">
+                          R$ {Number(product.basePrice).toFixed(2)}
+                        </p>
+                      )}
+
+                      <p>R$ {totalPrice}</p>
+                    </div>
+
+                    <Image
+                      width={0}
+                      height={0}
+                      sizes="100vw"
+                      className="w-full h-52 object-contain"
+                      src={product.imageUrls[0]}
+                      alt={product.name}
+                    />
+
+                    <div className="flex items-center justify-between gap-2 w-full">
+                      {product.discountPercentage !== 0 && (
+                        <p>
+                          <strong>{product.discountPercentage}%</strong> Desc
+                        </p>
+                      )}
+
+                      <p>
+                        Qtd: <strong>{product.quantity}</strong>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
+        </div>
+        {instanceRef.current && (
+          <div className="flex absolute max-md:hidden top-0 w-full h-full items-center justify-between">
+            <div className="w-20 h-full bg-gradient-to-r from-amber-100 flex">
+              <Arrow
+                left
+                onClick={(e) =>
+                  e.stopPropagation() || instanceRef.current?.prev()
+                }
+                disabled={currentSlide === 0}
+              />
+            </div>
+
+            <div className="w-20 h-full bg-gradient-to-l from-amber-100 flex">
+              <Arrow
+                onClick={(e) =>
+                  e.stopPropagation() || instanceRef.current?.next()
+                }
+                disabled={
+                  currentSlide ===
+                  instanceRef.current.track.details.slides.length - 1
+                }
+              />
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+function Arrow(props: {
+  disabled: boolean
+  left?: boolean
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onClick: (e: any) => void
+}) {
+  const disabled = props.disabled ? 'arrow--disabled' : ''
+
+  return (
+    <svg
+      onClick={props.onClick}
+      className={`arrow text-amber-500 opacity-60 hover:text-amber-800 duration-700 z-20 ${
+        props.left ? 'arrow--left' : 'arrow--right'
+      } ${disabled}`}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      {props.left && <ChevronLeft />}
+      {!props.left && <ChevronRight />}
+    </svg>
+  )
+}
