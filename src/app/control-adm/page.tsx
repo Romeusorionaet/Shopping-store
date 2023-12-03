c879f530124cc5c03c5161440a188f6def2e4214
romeusorionaet <romeusoares14569@gmail.com>
2023-12-03 11:34:18 -0300

    chore: turn in ssr logic for get-data in page-control

--- src/app/control-adm/page.tsx
+++ src/app/control-adm/page.tsx
@@ -5,7 +5,11 @@ import { AreaUpdateCategory } from './components/area-update-category'
 import { getDataProducts } from '@/lib/getData/get-data-products'
 import { AreaUpdateProduct } from './components/area-update-product'
 import { Category, Product } from '@prisma/client'
-import { ManageOrders } from './components/manager-orders'
+import {
+  AreaOrdersOfClients,
+  UserWithOrders,
+} from './components/area-orders-of-clients'
+import { getDataOrdersUsers } from '@/lib/getData/get-data-orders-users'
 
 export default async function ControlAdm() {
   const { propsCategories } = await getDataCatalog()
@@ -13,6 +17,77 @@ export default async function ControlAdm() {
 
   const { props } = await getDataProducts()
   const products: Product[] = JSON.parse(props.products)
+
+  const { propsOrdersUsers } = await getDataOrdersUsers()
+  const ordersUsers: UserWithOrders[] = JSON.parse(propsOrdersUsers.ordersUsers)
+
+  const completedPaymentUsers = ordersUsers.map((orderUser) => ({
+    ...orderUser,
+    Order: orderUser.Order.filter(
+      (order) =>
+        order.status === 'PAYMENT_CONFIRMED' &&
+        order.trackingCode === '' &&
+        order.orderTracking === 'WAITING',
+    ).sort((a, b) => {
+      const dateA = new Date(a.createdAt)
+      const dateB = new Date(b.createdAt)
+
+      return dateA.getTime() - dateB.getTime()
+    }),
+  }))
+
+  const historicOfCompletedPaymentUsers = ordersUsers.map((orderUser) => ({
+    ...orderUser,
+    Order: orderUser.Order.filter(
+      (order) =>
+        order.status === 'PAYMENT_CONFIRMED' &&
+        order.trackingCode !== '' &&
+        order.orderTracking === 'PRODUCT_DELIVERED_TO_CORREIOS',
+    ).sort((a, b) => {
+      const dateA = new Date(a.createdAt)
+      const dateB = new Date(b.createdAt)
+
+      return dateA.getTime() - dateB.getTime()
+    }),
+  }))
+
+  const historicOfOrderDeliveredToClient = ordersUsers.map((orderUser) => ({
+    ...orderUser,
+    Order: orderUser.Order.filter(
+      (order) => order.orderTracking === 'PRODUCT_DELIVERED_TO_CLIENT',
+    ).sort((a, b) => {
+      const dateA = new Date(a.createdAt)
+      const dateB = new Date(b.createdAt)
+
+      return dateA.getTime() - dateB.getTime()
+    }),
+  }))
+
+  const uncompletedPaymentUsers = ordersUsers.map((orderUser) => ({
+    ...orderUser,
+    Order: orderUser.Order.filter(
+      (order) => order.status === 'WAITING_FOR_PAYMENT',
+    ).sort((a, b) => {
+      const dateA = new Date(a.createdAt)
+      const dateB = new Date(b.createdAt)
+
+      return dateB.getTime() - dateA.getTime()
+    }),
+  }))
+
+  const historicOfOrdersCanceled = ordersUsers.map((orderUser) => ({
+    ...orderUser,
+    Order: orderUser.Order.filter(
+      (order) =>
+        order.status === 'PAYMENT_CONFIRMED' &&
+        order.orderTracking === 'CANCELED',
+    ).sort((a, b) => {
+      const dateA = new Date(a.createdAt)
+      const dateB = new Date(b.createdAt)
+
+      return dateA.getTime() - dateB.getTime()
+    }),
+  }))
 
   return (
     <main className="p-4 max-w-[800px] mx-auto">
@@ -37,7 +112,47 @@ export default async function ControlAdm() {
         Gerenciar pedidos
       </h2>
 
-      <ManageOrders />
+      <div className="space-y-10">
+        <div>
+          <h3>
+            Pedidos <strong className="text-green-500">pago</strong>,{' '}
+            <span className="text-sm text-zinc-300">preparar para entrega</span>
+          </h3>
+          <AreaOrdersOfClients ordersUsers={completedPaymentUsers} />
+        </div>
+
+        <div>
+          <h3>
+            Histórico de Pedidos{' '}
+            <strong className="text-green-500">pago</strong>,{' '}
+            <span className="text-sm text-zinc-300">a caminho</span>.
+          </h3>
+          <AreaOrdersOfClients ordersUsers={historicOfCompletedPaymentUsers} />
+        </div>
+
+        <div>
+          <h3>
+            Pedidos <strong className="text-green-500">entregue</strong>
+          </h3>
+          <AreaOrdersOfClients ordersUsers={historicOfOrderDeliveredToClient} />
+        </div>
+
+        <div>
+          <h3>
+            Pedidos <strong className="text-red-500">pendente</strong>,{' '}
+            <span className="text-sm text-zinc-300">desistente</span>.
+          </h3>
+          <AreaOrdersOfClients ordersUsers={uncompletedPaymentUsers} />
+        </div>
+
+        <div>
+          <h3>
+            Pedidos <strong className="text-red-500">cancelado</strong>,{' '}
+            <span className="text-sm text-zinc-300">reembolso</span>.
+          </h3>
+          <AreaOrdersOfClients ordersUsers={historicOfOrdersCanceled} />
+        </div>
+      </div>
     </main>
   )
 }
