3563768b0d92a63b1fd6c70e40357a6eb75d6fb8
romeusorionaet <romeusoares14569@gmail.com>
2024-05-16 15:28:39 -0300

    try login with google - 2.0

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,20 +1,20 @@
 import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google'
 import { AuthOptions } from 'next-auth'
-// import { api } from './api'
-// import { cookies } from 'next/headers'
-// import jwt, { JwtPayload } from 'jsonwebtoken'
+import { api } from './api'
+import { cookies } from 'next/headers'
+import jwt, { JwtPayload } from 'jsonwebtoken'
 
-// interface DecodedAccessToken extends JwtPayload {
-//   exp: number
-// }
+interface DecodedAccessToken extends JwtPayload {
+  exp: number
+}
 
 console.log(process.env.NEXTAUTH_URL, '=====vv')
 
 export const authOptions: AuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET_ID,
+      clientId: process.env.NEXTAUTH_GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.NEXTAUTH_GOOGLE_CLIENT_SECRET_ID || '',
       authorization: {
         params: {
           prompt: 'consent',
@@ -23,40 +23,48 @@ export const authOptions: AuthOptions = {
           redirect_uri: process.env.NEXTAUTH_URL,
         },
       },
-      profile(profile: GoogleProfile) {
+      async profile(profile: GoogleProfile) {
         if (profile.email_verified) {
-          // const response = await api.post(
-          //   '/auth/register/oauth-google/callback',
-          //   { profile },
-          // )
-          // const accessToken = response.data.accessToken
-          // const decodedAccessToken = jwt.decode(
-          //   accessToken,
-          // ) as DecodedAccessToken
-          // const accessTokenExpires = decodedAccessToken.exp
-          // const refreshToken = response.data.refreshToken
-          // const decodedRefreshToken = jwt.decode(
-          //   refreshToken,
-          // ) as DecodedAccessToken
-          // const refreshTokenExpires = decodedRefreshToken.exp
-          // cookies().set({
-          //   name: '@shopping-store/AT.2.0',
-          //   value: accessToken,
-          //   maxAge: accessTokenExpires - Math.floor(Date.now() / 1000),
-          //   httpOnly: true,
-          //   sameSite: 'lax',
-          //   secure: true,
-          //   path: '/',
-          // })
-          // cookies().set({
-          //   name: '@shopping-store/RT.2.0',
-          //   value: refreshToken,
-          //   maxAge: refreshTokenExpires - Math.floor(Date.now() / 1000),
-          //   httpOnly: true,
-          //   sameSite: 'lax',
-          //   secure: true,
-          //   path: '/',
-          // })
+          const response = await api.post(
+            '/auth/register/oauth-google/callback',
+            { profile },
+          )
+
+          const accessToken = response.data.accessToken
+
+          const decodedAccessToken = jwt.decode(
+            accessToken,
+          ) as DecodedAccessToken
+
+          const accessTokenExpires = decodedAccessToken.exp
+
+          const refreshToken = response.data.refreshToken
+
+          const decodedRefreshToken = jwt.decode(
+            refreshToken,
+          ) as DecodedAccessToken
+
+          const refreshTokenExpires = decodedRefreshToken.exp
+
+          cookies().set({
+            name: '@shopping-store/AT.2.0',
+            value: accessToken,
+            maxAge: accessTokenExpires - Math.floor(Date.now() / 1000),
+            httpOnly: true,
+            sameSite: 'lax',
+            secure: true,
+            path: '/',
+          })
+
+          cookies().set({
+            name: '@shopping-store/RT.2.0',
+            value: refreshToken,
+            maxAge: refreshTokenExpires - Math.floor(Date.now() / 1000),
+            httpOnly: true,
+            sameSite: 'lax',
+            secure: true,
+            path: '/',
+          })
         }
 
         return {
