740deeadeaf321c4131892b3498ae9731025827b
romeusorionaet <romeusoares14569@gmail.com>
2024-05-17 00:25:42 -0300

    fix type signature error

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,21 +1,97 @@
-export const getGoogleOAuthURL = () => {
-  const rootUrl = 'https://accounts.google.com/o/oauth2/v2/auth'
+import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google'
+import { cookies } from 'next/headers'
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import { api } from '@/lib/api'
+import { AuthOptions } from 'next-auth'
 
-  const options = {
-    redirect_uri:
-      'https://api-shopping-store.onrender.com/auth/register/oauth-google/callback',
-    client_id:
-      '581486127159-kv2ul4b30dpj1blp9na91rqf6974pba0.apps.googleusercontent.com',
-    access_type: 'offline',
-    response_type: 'code',
-    prompt: 'consent',
-    scope: [
-      'https://www.googleapis.com/auth/userinfo.profile',
-      'https://www.googleapis.com/auth/userinfo.email',
-    ].join(' '),
-  }
-  const qs = new URLSearchParams(options)
-  const url = `${rootUrl}?${qs.toString()}`
-
-  window.open(url, '_self')
+interface DecodedAccessToken extends JwtPayload {
+  exp: number
 }
+
+export const authOptions: AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.NEXTAUTH_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.NEXTAUTH_GOOGLE_CLIENT_SECRET_ID,
+      authorization: {
+        url: 'http://www.google.com/oauth/v2/accessToken',
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+          scope: [
+            'https://www.googleapis.com/auth/userinfo.profile',
+            'https://www.googleapis.com/auth/userinfo.email',
+          ].join(' '),
+          include_granted_scopes: 'true',
+        },
+      },
+      async profile(profile: GoogleProfile) {
+        if (profile.email_verified) {
+          const response = await api.post(
+            '/auth/register/oauth-google/callback',
+            { profile },
+          )
+
+          const accessToken = response.data.accessToken
+
+          const decodedAccessToken = jwt.decode(
+            accessToken,
+          ) as DecodedAccessToken
+
+          const accessTokenExpires = decodedAccessToken.exp
+
+          const refreshToken = response.data.refreshToken
+
+          const decodedRefreshToken = jwt.decode(
+            refreshToken,
+          ) as DecodedAccessToken
+
+          const refreshTokenExpires = decodedRefreshToken.exp
+
+          cookies().set({
+            name: '@shopping-store/AT.2.0',
+            value: accessToken,
+            maxAge: accessTokenExpires - Math.floor(Date.now() / 1000),
+            httpOnly: true,
+            sameSite: 'lax',
+            secure: true,
+            path: '/',
+          })
+
+          cookies().set({
+            name: '@shopping-store/RT.2.0',
+            value: refreshToken,
+            maxAge: refreshTokenExpires - Math.floor(Date.now() / 1000),
+            httpOnly: true,
+            sameSite: 'lax',
+            secure: true,
+            path: '/',
+          })
+        }
+
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+        }
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session }) {
+      session.user = { ...session.user } as {
+        id: string
+        name: string
+        email: string
+        image: string
+      }
+
+      return session
+    },
+  },
+}
+
+// olhar o repositório do rapaz e ver qual foi o geito que ele fez a config de auth google
