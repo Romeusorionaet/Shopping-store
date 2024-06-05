37b28d9ed06c1238b3bdbc85f560d79a8260a829
romeusorionaet <romeusoares14569@gmail.com>
2024-06-05 13:41:52 -0300

    refactor, avoid cod repeat

--- src/app/(store)/details/components/technical-product-details.tsx
+++ src/app/(store)/details/components/technical-product-details.tsx
@@ -5,6 +5,38 @@ interface Props {
 }
 
 export function TechnicalProductDetails({ technicalProductDetails }: Props) {
+  const productDetails = [
+    { label: 'Marca', value: technicalProductDetails.brand },
+    { label: 'Modelo', value: technicalProductDetails.model },
+    { label: 'RAM', value: technicalProductDetails.ram },
+    { label: 'ROM', value: technicalProductDetails.rom },
+    { label: 'Processador', value: technicalProductDetails.processorBrand },
+    { label: 'Tela', value: technicalProductDetails.screenOrWatchFace },
+    { label: 'Largura', value: technicalProductDetails.width },
+    { label: 'Altura', value: technicalProductDetails.height },
+    { label: 'Peso', value: technicalProductDetails.weight },
+    {
+      label: 'Resolução de captura de vídeo',
+      value: technicalProductDetails.videoCaptureResolution,
+    },
+    {
+      label: 'Resolução de vídeo',
+      value: technicalProductDetails.videoResolution,
+    },
+    {
+      label: 'Tempo médio da bateria',
+      value: technicalProductDetails.averageBatteryLife,
+    },
+    {
+      label: 'Capacidade da bateria',
+      value: technicalProductDetails.batteryCapacity,
+    },
+    {
+      label: 'Sistema operacional',
+      value: technicalProductDetails.operatingSystem,
+    },
+  ]
+
   return (
     <div className="mb-28 ml-2">
       <h2 className="text-lg uppercase md:text-2xl">
@@ -12,76 +44,15 @@ export function TechnicalProductDetails({ technicalProductDetails }: Props) {
       </h2>
 
       <div className="mt-8 flex flex-col gap-4 text-xs md:text-base">
-        <div className="flex items-center gap-4">
-          <span>Marca</span>
-          <p className="bg-zinc-100 p-1">{technicalProductDetails.brand}</p>
-        </div>
-        <div className="flex items-center gap-4">
-          <span>Modelo</span>
-          <p className="bg-zinc-100 p-1">{technicalProductDetails.model}</p>
-        </div>
-        <div className="flex items-center gap-4">
-          <span>Tempo médio da bateria</span>
-          <p className="bg-zinc-100 p-1">
-            {technicalProductDetails.averageBatteryLife}
-          </p>
-        </div>
-        <div className="flex items-center gap-4">
-          <span>Capacidade da bateria</span>
-          <p className="bg-zinc-100 p-1">
-            {technicalProductDetails.batteryCapacity}
-          </p>
-        </div>
-        <div className="flex items-center gap-4">
-          <span>Largura</span>
-          <p className="bg-zinc-100 p-1">{technicalProductDetails.width}</p>
-        </div>
-        <div className="flex items-center gap-4">
-          <span>Altura</span>
-          <p className="bg-zinc-100 p-1">{technicalProductDetails.height}</p>
-        </div>
-        <div className="flex items-center gap-4">
-          <span>Peso</span>
-          <p className="bg-zinc-100 p-1">{technicalProductDetails.weight}</p>
-        </div>
-        <div className="flex items-center gap-4">
-          <span>RAM</span>
-          <p className="bg-zinc-100 p-1">{technicalProductDetails.ram}</p>
-        </div>
-        <div className="flex items-center gap-4">
-          <span>ROM</span>
-          <p className="bg-zinc-100 p-1">{technicalProductDetails.rom}</p>
-        </div>
-        <div className="flex items-center gap-4">
-          <span>Resolução de vídeo</span>
-          <p className="bg-zinc-100 p-1">
-            {technicalProductDetails.videoResolution}
-          </p>
-        </div>
-        <div className="flex items-center gap-4">
-          <span>Resolução de captura de vídeo</span>
-          <p className="bg-zinc-100 p-1">
-            {technicalProductDetails.videoCaptureResolution}
-          </p>
-        </div>
-        <div className="flex items-center gap-4">
-          <span>Tela</span>
-          <p className="bg-zinc-100 p-1">
-            {technicalProductDetails.screenOrWatchFace}
-          </p>
-        </div>
-        <div className="flex items-center gap-4">
-          <span>Processador</span>
-          <p className="bg-zinc-100 p-1">
-            {technicalProductDetails.processorBrand}
-          </p>
-        </div>
-        <div className="flex items-center gap-4">
-          <span>Sistema operacional</span>
-          <p className="bg-zinc-100 p-1">
-            {technicalProductDetails.operatingSystem}
-          </p>
-        </div>
+        {productDetails.map(
+          (detail, index) =>
+            detail.value && (
+              <div key={index} className="flex items-center gap-4">
+                <span>{detail.label}</span>
+                <p className="bg-zinc-100 p-1">{detail.value}</p>
+              </div>
+            ),
+        )}
       </div>
     </div>
   )
