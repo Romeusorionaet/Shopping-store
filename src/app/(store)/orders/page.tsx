2eade2bb489a9f6b19fbf7a4b033d679a61e8fa4
romeusorionaet <romeusoares14569@gmail.com>
2024-06-06 21:52:48 -0300

    moved function to folder home for test

--- src/app/(store)/orders/page.tsx
+++ src/app/(store)/orders/page.tsx
@@ -18,6 +18,10 @@ export default async function Orders() {
   const { props } = await getDataBuyerOrders()
   const orders: OrderProps[] = JSON.parse(props.orders)
 
+  if (!orders) {
+    return <p className="pt-52 text-center">Realize uma compra...</p>
+  }
+
   const paymentConfirmed = OrderStatus.PAYMENT_CONFIRMED
   const ordersInProcess =
     OrderStatusTracking.WAITING ||
