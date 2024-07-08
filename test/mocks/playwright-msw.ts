cdee6f9580f6b0c4530a47933301b21c323f540d
romeusorionaet <romeusoares14569@gmail.com>
2024-07-08 11:48:35 -0300

    fixed: resume path

--- test/mocks/playwright-msw.ts
+++ test/mocks/playwright-msw.ts
@@ -2,7 +2,7 @@ import { test as base, expect } from '@playwright/test'
 import { http } from 'msw'
 import type { MockServiceWorker } from 'playwright-msw'
 import { createWorkerFixture } from 'playwright-msw'
-import { handlers } from './handlers/index'
+import { handlers } from './handlers'
 
 const test = base.extend<{
   worker: MockServiceWorker
