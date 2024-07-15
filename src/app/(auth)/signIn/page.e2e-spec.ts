8dd73bb4a1b0c15d5d5edd5c5cf3ebf14fd95129
romeusorionaet <romeusoares14569@gmail.com>
2024-07-15 15:27:34 -0300

    updated: uploadthing / created test e2e for signIn

---
+++ src/app/(auth)/signIn/page.e2e-spec.ts
@@ -0,0 +1,39 @@
+import { HttpResponse } from 'msw'
+import { test, expect } from '../../../../test/mocks/playwright-msw'
+import { makeUser } from '../../../../test/factories/make-user'
+
+test.describe('SignIn Test (E2E)', () => {
+  test('should be able the user sign in', async ({ page, worker, http }) => {
+    await page.goto('/signIn')
+
+    const user = makeUser({
+      username: 'Romeu soares',
+      email: 'romeu@gmail.com',
+    })
+
+    await worker.use(
+      http.get('/buyer/profile', async () => {
+        return new HttpResponse(
+          JSON.stringify({
+            profile: user,
+          }),
+          {
+            status: 200,
+          },
+        )
+      }),
+    )
+
+    await page.getByPlaceholder('pedro@gmail.com').fill(user.email)
+    await page.getByLabel('Senha', { exact: true }).fill('123456')
+    await page.getByRole('button', { name: 'Entrar', exact: true }).click()
+
+    await page.waitForLoadState('networkidle')
+
+    expect(page.waitForURL('/')).toBeTruthy()
+
+    const userName = page.locator(user.username)
+
+    expect(userName).toBeTruthy()
+  })
+})
